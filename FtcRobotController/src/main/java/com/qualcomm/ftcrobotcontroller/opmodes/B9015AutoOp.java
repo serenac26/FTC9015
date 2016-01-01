ba1e56c6070c4e9a5eaf0d891937de6e29afad9a
Serena <serenacheng99@hotmail.com>
2015-12-31 21:50:44 -0600

    start auto

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/B9015AutoOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/B9015AutoOp.java
@@ -4,5 +4,22 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
  * Created by seren_000 on 12/29/2015.
  */
 public class B9015AutoOp extends B9015BaseOp {
+    public B9015AutoOp(){
 
+    }
+
+    @Override
+    public void init(){
+        super.init();
+    }
+
+    @Override
+    public void loop(){
+
+    }
+
+    @Override
+    public void stop(){
+
+    }
 }
