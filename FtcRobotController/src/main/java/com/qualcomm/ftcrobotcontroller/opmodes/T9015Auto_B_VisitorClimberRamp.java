ae98909dea270403fbcc94f269ecb1673779d4e5
serenac26 <no.1tennis@gmail.com>
2016-01-14 19:42:44 -0600

    Update T9015Auto_B_VisitorClimberRamp.java

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/T9015Auto_B_VisitorClimberRamp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/T9015Auto_B_VisitorClimberRamp.java
@@ -49,7 +49,7 @@ public class T9015Auto_B_VisitorClimberRamp extends T9015Hardware {
                 distance = 130; //set distance to move (cm)
                 power    = 0.3; //set power
                 telemetry.addData("1 - ", "forward=" + distance + "p=" + power); //displays distance and power to phone screen
-                if (has_driver_forward_cm(distance, power)) //when encoder has reached corresponding ticks for set distance move to the next state
+                if (has_driver_moved_cm(distance, power)) //when encoder has reached corresponding ticks for set distance move to the next state
                     move_to_next_state();
                 break;
             case 2:
@@ -83,7 +83,7 @@ public class T9015Auto_B_VisitorClimberRamp extends T9015Hardware {
                 distance = 10;  //set distance to move (cm)
                 power    = 0.3;  //set power
                 telemetry.addData("3 - ","forward=" + distance + "p=" + power); //displays distance and power to phone screen
-                if (has_driver_forward_cm(distance, power)) //when encoder has reached corresponding ticks for set distance move to the next state
+                if (has_driver_moved_cm(distance, power)) //when encoder has reached corresponding ticks for set distance move to the next state
                     move_to_next_state();
                 break;
             case 6:
@@ -122,7 +122,7 @@ public class T9015Auto_B_VisitorClimberRamp extends T9015Hardware {
                 distance = 120;  //set distance to move (cm)
                 power    = 0.3;  //set power
                 telemetry.addData("3 - ","backward=" + distance + "p=" + power); //displays distance and power to phone screen
-                if (has_driver_forward_cm(distance, power)) //when encoder has reached corresponding ticks for set distance move to the next state
+                if (has_driver_moved_cm(distance, power)) //when encoder has reached corresponding ticks for set distance move to the next state
                     move_to_next_state();
                 break;
             case 12:
@@ -157,7 +157,7 @@ public class T9015Auto_B_VisitorClimberRamp extends T9015Hardware {
                 distance = 120;  //set distance to move (cm)
                 power    = 0.8;  //set power
                 telemetry.addData("6 - ","backward=" + distance + "p=" + power); //displays distance and power to phone screen
-                if (has_driver_forward_cm(distance, power)) //when encoder has reached corresponding ticks for set distance move to the next state
+                if (has_driver_moved_cm(distance, power)) //when encoder has reached corresponding ticks for set distance move to the next state
                     move_to_next_state(); //moves to final default state and ends program
                 break;
             default:
